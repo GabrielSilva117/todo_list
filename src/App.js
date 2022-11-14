ecefba641611665688c351e6cf5777e84e040cd3
GabrielSilva117 <gabriel.f.silva117@gmail.com>
2022-11-14 10:11:28 -0300

    feat: Added a App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [ tasks, setTasks ] = useState([
+    {
+      id: 1,
+      text: 'Morning jog',
+      reminde: true
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
     </div>
-  );
+  )
 }
 
 export default App;
